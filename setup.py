e25021dd643f4b0cf9fe767b8e936a58645e33f8
Jonathon J Howey <jonathon.howey@goexpedi.com>
2018-07-26 21:23:50 -0500

    Setup and pipenv upgrades

--- setup.py
+++ setup.py
@@ -16,10 +16,13 @@ setuptools.setup(
     classifiers=(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Topic :: Software Development :: Libraries :: Python Modules"
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ),
+    setup_requires=["pbr"],
     tests_require=["pytest"],
+    pbr=True,
 )
